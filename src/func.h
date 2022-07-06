9503b05a1b23ae17dca8ac02af3d6a41ada0478c
du2693742095 <dushiyang68@gmail.com>
2022-07-06 21:04:52 +0800

    create

---
+++ src/func.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+#include <stdbool.h>
+
+#include <dirent.h> //directorys
+#include <time.h>   // localtime
+
+#include <sys/types.h>
+#include <sys/stat.h>   //stat
+#include <fcntl.h>      //unbuffered I/O
+#include <sys/time.h>   //select
+#include <sys/select.h> //select
+#include <unistd.h>     //nubuffered I/O
+#include <sys/mman.h>   //mmap
+#include <sys/wait.h>   //wait
+#include <signal.h>     //signal, sigaction
+
+#define Error_Equal_Check(data, standard, input)\
+    if(data == standard){\
+        perror(input);\
+        exit(1);\
+    }
+
+#define Error_Unequal_Check(data, standard, input)\
+    if(data != standard) {\
+        puts(input);\
+        exit(1);\
+    }
+
+typedef __sighandler_t sighandler_t;
